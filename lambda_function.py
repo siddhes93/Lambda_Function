f4dd274015a6d87dec48cedb09490e2c65088139
siddhes <103021940+siddhes93@users.noreply.github.com>
2022-10-09 12:09:39 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,8 @@
-import boto3
+import boto3 ##Boto 3 is SDK for python
 
 client = boto3.client('sns')
 
 def lambda_handler(event, context):
-    topic_arn='arn:aws:sns:us-east-1:*******:Prodalert'
-    message='prod server stopped alerts please look into it'
-    client.publish(TopicArn=topic_arn,Message=message)
+    topic_arn='arn:aws:sns:us-east-1:*******:Prodalert'  ## in Place of *** use your ARN accoutn ID##
+    message='prod server stopped alerts please look into it' ## you can add your own custom message. these are varables which is used down.
+    client.publish(TopicArn=topic_arn,Message=message) 
